1d77e54730e99ace228e4b326639a075395a3dc1
Svetozar Ikovic <ikovic.svetozar@gmail.com>
2026-06-13 10:34:19 +0200

    Implement imports, assignments, and class declarations

--- LINVAST.Imperative/Builders/Python/PythonASTBuilder.Types.cs
+++ LINVAST.Imperative/Builders/Python/PythonASTBuilder.Types.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Collections.Generic;
+using System.Linq;
 using LINVAST.Builders;
 using LINVAST.Imperative.Nodes;
 using LINVAST.Nodes;
@@ -8,7 +9,39 @@ namespace LINVAST.Imperative.Builders.Python
     public sealed partial class PythonASTBuilder : Python3ParserBaseVisitor<ASTNode>, IASTBuilder<Python3Parser>
     {
         // classdef: 'class' name ('(' arglist? ')')? ':' block
-        public override ASTNode VisitClassdef(Python3Parser.ClassdefContext ctx) =>
-            throw new NotImplementedException("classdef");
+        public override ASTNode VisitClassdef(Python3Parser.ClassdefContext ctx)
+        {
+            var className = new IdNode(ctx.Start.Line, ctx.name().GetText());
+
+            var baseTypes = new TypeNameListNode(ctx.Start.Line);
+            if (ctx.arglist() is not null) {
+                var baseTypeNames = ctx.arglist().argument()
+                    .Where(a => a.GetText().IndexOf('=') < 0)
+                    .Select(a => new TypeNameNode(a.Start.Line, a.GetText()));
+                baseTypes = new TypeNameListNode(ctx.Start.Line, baseTypeNames);
+            }
+
+            ASTNode body = this.Visit(ctx.block());
+            IEnumerable<ASTNode> bodyChildren = body is BlockStatNode block
+                ? block.Children
+                : new[] { body };
+            IReadOnlyList<ASTNode> processed = this.AddDeclarations(bodyChildren);
+
+            var declarations = new List<DeclStatNode>();
+            foreach (ASTNode child in processed) {
+                if (child is DeclStatNode declStat)
+                    declarations.Add(declStat);
+                else if (child is FuncNode funcNode)
+                    declarations.Add(funcNode);
+                else
+                    declarations.Add(new DeclStatNode(child.Line, new DeclSpecsNode(child.Line),
+                        new DeclListNode(child.Line)));
+            }
+
+            var templateParams = new TypeNameListNode(ctx.Start.Line);
+            var declSpecs = new DeclSpecsNode(ctx.Start.Line);
+            var typeDecl = new TypeDeclNode(ctx.Start.Line, className, templateParams, baseTypes, declarations);
+            return new ClassNode(ctx.Start.Line, declSpecs, typeDecl);
+        }
     }
 }
