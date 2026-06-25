e59c128a3e6f1e0e5292077d2c9d93eb96f887b7
Svetozar Ikovic <ikovic.svetozar@gmail.com>
2026-06-25 22:22:17 +0200

    Add more tests

--- LINVAST.Tests/Imperative/Builders/Python/DeclarationTests.cs
+++ LINVAST.Tests/Imperative/Builders/Python/DeclarationTests.cs
@@ -101,9 +101,23 @@ namespace LINVAST.Tests.Imperative.Builders.Python
             var declStat = nodes.Single().As<DeclStatNode>();
             var varDecls = declStat.DeclaratorList.Declarators.ToList();
 
-            Assert.That(varDecls.Count(), Is.EqualTo(2));
+            Assert.That(varDecls.Count, Is.EqualTo(2));
             Assert.That(varDecls[0].As<VarDeclNode>().Identifier, Is.EqualTo("a"));
+            Assert.That(varDecls[0].As<VarDeclNode>().Initializer!.As<LitExprNode>().Value, Is.EqualTo(1));
             Assert.That(varDecls[1].As<VarDeclNode>().Identifier, Is.EqualTo("b"));
+            Assert.That(varDecls[1].As<VarDeclNode>().Initializer!.As<LitExprNode>().Value, Is.EqualTo(2));
+        }
+        [Test]
+        public void TupleUnpackingSimpleLong()
+        {
+            var source = "a, b, c, d, e = 1, 2, 3, 4, 5\n";
+            var nodes = this.builder.BuildFromSource(source).As<SourceNode>().Children;
+
+            Assert.That(nodes.Single(), Is.TypeOf<DeclStatNode>());
+            var declStat = nodes.Single().As<DeclStatNode>();
+            var varDecls = declStat.DeclaratorList.Declarators.ToList();
+
+            Assert.That(varDecls.Count, Is.EqualTo(5));
         }
 
         [Test]
@@ -113,7 +127,9 @@ namespace LINVAST.Tests.Imperative.Builders.Python
             var nodes = this.builder.BuildFromSource(source).As<SourceNode>().Children;
 
             Assert.That(nodes.Single(), Is.TypeOf<ExprStatNode>());
-            Assert.That(nodes.Single().As<ExprStatNode>().Expression, Is.TypeOf<AssignExprNode>());
+            var assign = nodes.Single().As<ExprStatNode>().Expression.As<AssignExprNode>();
+            Assert.That(assign.LeftOperand.As<ArrInitExprNode>().Expressions.Count(), Is.EqualTo(3));
+            Assert.That(assign.RightOperand.As<ArrInitExprNode>().Expressions.Count(), Is.EqualTo(2));
         }
 
         [Test]
@@ -123,9 +139,17 @@ namespace LINVAST.Tests.Imperative.Builders.Python
             var nodes = this.builder.BuildFromSource(source).As<SourceNode>().Children.ToList();
 
             Assert.That(nodes.Count, Is.EqualTo(2));
-            Assert.That(nodes[0], Is.TypeOf<DeclStatNode>());
-            Assert.That(nodes[1], Is.TypeOf<ExprStatNode>());
-            Assert.That(nodes[1].As<ExprStatNode>().Expression, Is.TypeOf<AssignExprNode>());
+
+            var decl = nodes[0].As<DeclStatNode>();
+            Assert.That(decl.Specifiers.TypeName, Is.EqualTo("int"));
+            Assert.That(decl.DeclaratorList.Declarators.Single().As<VarDeclNode>().Identifier, Is.EqualTo("a"));
+            Assert.That(decl.DeclaratorList.Declarators.Single().As<VarDeclNode>().Initializer!.As<LitExprNode>().Value, Is.EqualTo(0));
+
+            var assign = nodes[1].As<ExprStatNode>().Expression.As<AssignExprNode>();
+            Assert.That(assign.LeftOperand.As<ArrInitExprNode>().Expressions.Select(e => e.As<IdNode>().Identifier),
+                Is.EqualTo(new[] { "a", "b" }));
+            Assert.That(assign.RightOperand.As<ArrInitExprNode>().Expressions.Select(e => e.As<LitExprNode>().Value),
+                Is.EqualTo(new object[] { 1, 2 }));
         }
 
         [Test]
@@ -135,9 +159,34 @@ namespace LINVAST.Tests.Imperative.Builders.Python
             var nodes = this.builder.BuildFromSource(source).As<SourceNode>().Children.ToList();
 
             Assert.That(nodes.Count, Is.EqualTo(2));
-            Assert.That(nodes[0], Is.TypeOf<DeclStatNode>());
-            Assert.That(nodes[1], Is.TypeOf<ExprStatNode>());
-            Assert.That(nodes[1].As<ExprStatNode>().Expression, Is.TypeOf<AssignExprNode>());
+
+            var decl = nodes[0].As<DeclStatNode>();
+            Assert.That(decl.Specifiers.TypeName, Is.EqualTo("int"));
+            Assert.That(decl.DeclaratorList.Declarators.Single().As<VarDeclNode>().Identifier, Is.EqualTo("count"));
+            Assert.That(decl.DeclaratorList.Declarators.Single().As<VarDeclNode>().Initializer!.As<LitExprNode>().Value, Is.EqualTo(1));
+
+            var assign = nodes[1].As<ExprStatNode>().Expression.As<AssignExprNode>();
+            Assert.That(assign.LeftOperand.As<IdNode>().Identifier, Is.EqualTo("count"));
+            Assert.That(assign.RightOperand.As<LitExprNode>().Value, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TupleUnpackingAnnotatedTypeIsPromotedButTypeAnnotationIsLost()
+        {
+            var source = "(a, b): tuple = (1, 2)\n";
+            var nodes = this.builder.BuildFromSource(source).As<SourceNode>().Children.ToList();
+
+            Assert.That(nodes.Single(), Is.TypeOf<DeclStatNode>());
+            var declStat = nodes.Single().As<DeclStatNode>();
+
+            Assert.That(declStat.Specifiers.TypeName, Is.EqualTo("object"));
+
+            var varDecls = declStat.DeclaratorList.Declarators.ToList();
+            Assert.That(varDecls.Count, Is.EqualTo(2));
+            Assert.That(varDecls[0].As<VarDeclNode>().Identifier, Is.EqualTo("a"));
+            Assert.That(varDecls[0].As<VarDeclNode>().Initializer!.As<LitExprNode>().Value, Is.EqualTo(1));
+            Assert.That(varDecls[1].As<VarDeclNode>().Identifier, Is.EqualTo("b"));
+            Assert.That(varDecls[1].As<VarDeclNode>().Initializer!.As<LitExprNode>().Value, Is.EqualTo(2));
         }
     }
 }
